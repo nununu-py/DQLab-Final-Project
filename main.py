eaac3eed0e2192a118d8273b2ace3b74fdcc9b55
Nununu_py <104470984+nununu-py@users.noreply.github.com>
2023-01-09 19:11:28 +0700

    Update main.py

--- main.py
+++ main.py
@@ -372,7 +372,7 @@ with st.container():
             ##### Analysis Question 5. How many listings have reviews above the average per each \
             neighborhood group ?
             """)
-        col1, col2= st.columns(2, gap="large")
+        col1, col2, col3 = st.columns(3)
 
         with col1:
 
@@ -388,6 +388,9 @@ with st.container():
             st.plotly_chart(high_reviews_map)
         
         with col2:
+                pass
+        
+        with col3:
 
             data = high_reviews.groupby(['neighbourhood_group'])[
                 'name'].size().to_frame('Total Listing').reset_index()
